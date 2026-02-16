11e1d8360fd1eb830b685ea51871e62a83044f00
Rafa Bena <rbenrax@gmail.com>
2026-02-16 10:57:07 +0100

    Clening

--- target/debug/rserial.d
+++
@@ -1,1 +0,0 @@
-/home/rafa/Desarrollo/esp23-r/rserial/target/debug/rserial: /home/rafa/Desarrollo/esp23-r/rserial/src/main.rs
--- target/release/rserial.d
+++
@@ -1,1 +0,0 @@
-/home/rafa/Desarrollo/esp23-r/rserial/target/release/rserial: /home/rafa/Desarrollo/esp23-r/rserial/src/main.rs
